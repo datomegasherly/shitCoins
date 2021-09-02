0c7917f83e1572353f56d5bbe2ad8fe7a07e6407
datomegasherly <dat.omega.sherly@gmail.com>
2021-09-03 02:59:48 +0430

    first Release

---
+++ coin.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+class Coin {
+  constructor() {
+    this.connect();
+    this.models();
+  }
+  async connect() {
+    this.db = await mongoose.connect("mongodb://localhost:27017/coin", {
+      useNewUrlParser: true,
+    });
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.once("open", function () {
+      console.log("Connected successfully");
+    });
+  }
+  models() {
+    const schema = new mongoose.Schema({
+      name: mongoose.Schema.Types.String,
+      contract: mongoose.Schema.Types.String,
+      date: mongoose.Schema.Types.String,
+    });
+    this.model = mongoose.model("Coin", schema);
+  }
+  async create(data) {
+    const Coin = this.model;
+    const prevData = await Coin.findOneAndUpdate(
+      { contract: data.contract },
+      { name: data.name, date: data.date }
+    );
+    if (!prevData) {
+      await Coin.create(data);
+    }
+  }
+  async delete() {
+    const Coin = this.model;
+    await Coin.deleteMany({});
+  }
+  async get() {
+    const Coin = this.model;
+    return await Coin.find().sort({ date: "asc" });
+  }
+}
+
+module.exports = Coin;
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Coin = require("./coin");
+
+const port = 4040;
+
+const coin = new Coin();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/scripts"));
+app.set("view engine", "pug");
+
+app.get("/", async (req, res) => {
+  const data = await coin.get();
+  const countDays = (t) => {
+    var cd = 24 * 60 * 60 * 1000,
+      ch = 60 * 60 * 1000,
+      d = Math.floor(t / cd),
+      h = Math.floor((t - d * cd) / ch),
+      m = Math.round((t - d * cd - h * ch) / 60000),
+      pad = function (n) {
+        return n < 10 ? "0" + n : n;
+      };
+    if (m === 60) {
+      h++;
+      m = 0;
+    }
+    if (h === 24) {
+      d++;
+      h = 0;
+    }
+    return `${d} Days - ${pad(h)} Hours - ${pad(m)} Minutes`;
+  };
+  res.render("list", { data, countDays });
+});
+
+app.get("/create", (req, res) => {
+  res.render("create", { isPost: {} });
+});
+/*app.get("/deleteAll", (req, res) => {
+  coin.delete();
+  res.render("create", { isPost: {} });
+});*/
+app.post("/create", (req, res) => {
+  if (req.body.name === "") {
+    res.render("create", { isPost: { error: true } });
+    return;
+  }
+  if (req.body.contract === "") {
+    res.render("create", { isPost: { error: true } });
+    return;
+  }
+  if (req.body.date === "") {
+    res.render("create", { isPost: { error: true } });
+    return;
+  }
+  coin.create({
+    name: req.body.name,
+    contract: req.body.contract,
+    date: req.body.date,
+  });
+  res.render("create", { isPost: { success: true } });
+});
+
+app.listen(port, () => {
+  console.log(`The app is started on port ${port}`);
+});
