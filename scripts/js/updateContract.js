e37d12abb7479c44c89174909dc8d8c68fba86ba
datomegasherly <dat.omega.sherly@gmail.com>
2021-09-05 02:30:46 +0430

    move fetch from server to client

--- scripts/js/updateContract.js
+++ scripts/js/updateContract.js
@@ -3,7 +3,7 @@ setTimeout(() => {
 
   contract.addEventListener("blur", function (ev) {
     if (ev.target.value !== "") {
-      fetch(`/checkContract`, {
+      /*fetch(`/checkContract`, {
         method: "post",
         headers: {
           "Content-Type": "application/json",
@@ -13,7 +13,23 @@ setTimeout(() => {
         .then((response) => response.json())
         .then((data) => {
           document.querySelector("[name=name]").value = data;
-        });
+        });*/
+      try {
+        fetch(`https://api1.poocoin.app/tokens?search=${ev.target.value}`, {
+          //mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            document.querySelector("[name=name]").value = data[0]
+              ? `${data[0].name} (${data[0].symbol})`
+              : "";
+          });
+      } catch (err) {
+        return;
+      }
     }
   });
 }, 500);
